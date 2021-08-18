1a387a3f3598b6c263afd2a3b59165cdfed9ad86
deepvn98 <deepvn988@gmail.com>
2021-08-18 22:26:51 +0700

    add update movies

--- src/main/java/com/example/movie/controller/MovieController.java
+++ src/main/java/com/example/movie/controller/MovieController.java
@@ -1,5 +1,6 @@
 package com.example.movie.controller;
 
+import com.example.movie.dto.responsy.ResponMessage;
 import com.example.movie.model.Movie;
 import com.example.movie.service.iplm.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +8,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -25,7 +29,7 @@ public class MovieController {
     }
 
     @GetMapping
-    public ResponseEntity<?> pageSong(@PageableDefault(sort = "name", direction = Sort.Direction.ASC) Pageable pageable) {
+    public ResponseEntity<?> pageMovies(@PageableDefault(sort = "name", direction = Sort.Direction.ASC) Pageable pageable) {
         Page<Movie> moviesPage = movieService.getAll(pageable);
         if (moviesPage.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
@@ -33,7 +37,18 @@ public class MovieController {
         return new ResponseEntity<>(moviesPage, HttpStatus.OK);
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateMovies(@PathVariable Long id, @RequestBody Movie movie) {
+        Optional<Movie> movie1 = movieService.getById(id);
+        if (!movie1.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            movie.setId(id);
+            movieService.save(movie);
+            return new ResponseEntity<>(new ResponMessage("yes"), HttpStatus.OK);
+        }
 
+    }
 
 
 }
